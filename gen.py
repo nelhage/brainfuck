a4cddc0a935b84c74aeeffbe234cd3ca58fe9bb5
Nelson Elhage <nelhage@nelhage.com>
2014-05-10 09:11:52 -0700

    print character stats

--- gen.py
+++ gen.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import re
+from collections import defaultdict
 
 tail = open('tail.bf').read()
 t = re.sub(r'[^]+><.[-]', '', tail)
@@ -12,3 +13,10 @@ with open('quine.bf', 'w') as f:
     f.write(">>>>\n\n")
     f.write('>'.join(['+' * tbl[c] for c in t]) + '>' + "\n\n")
     f.write(tail)
+
+d = defaultdict(lambda: 0)
+for c in t:
+    d[c] += 1
+
+for k,v in sorted(d.items(), key=lambda p:p[1], reverse=True):
+    print " %s  %s" % (k, v)
