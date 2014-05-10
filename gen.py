222174c87c8544472e541985e46d976ca53f954b
Nelson Elhage <nelhage@nelhage.com>
2014-05-09 23:03:53 -0700

    1695

--- gen.py
+++ gen.py
@@ -10,5 +10,5 @@ tbl = {'+':1, '<': 2, '>': 3, '-':4, '[':5, ']':6, '.':7}
 
 with open('quine1.bf', 'w') as f:
     f.write(">>>>\n\n")
-    f.write('>>>>'.join(['+' * tbl[c] for c in t]) + '>>>>' + "\n\n")
+    f.write('>'.join(['+' * tbl[c] for c in t]) + '>' + "\n\n")
     f.write(tail)
